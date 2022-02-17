0ebc5ee198656e30bef5f0bf7f2261b1d574240d
hermkan <hermann.kanga.dev@gmail.com>
2022-02-17 00:35:19 -0500

    completed setup theming

---
+++ src/Infrastructure/theme/index.js
@@ -0,0 +1,19 @@
+import { colors } from "./colors";
+import { space, lineHeights } from "./spacing";
+import { sizes } from "./sizes";
+import { fonts, fontWeights, fontSizes } from "./fonts";
+import { shadow } from "./shadow";
+import { transition } from "./transition";
+import { radius } from "./radius";
+export const theme = {
+  colors,
+  space,
+  lineHeights,
+  sizes,
+  fonts,
+  fontSizes,
+  fontWeights,
+  shadow,
+  transition,
+  radius,
+};
